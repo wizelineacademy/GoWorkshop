7b7839e233d34c6e3cb6dc193f0afad7532a3dff
Alex Pliutau <a.pliutau@gmail.com>
2017-09-17 13:20:50 +0700

    Use sendgrid config

--- notifier/controllers/handlers.go
+++ notifier/controllers/handlers.go
@@ -1,10 +1,11 @@
 package controllers
 
 import (
+	"log"
+
 	"github.com/wizelineacademy/GoWorkshop/proto/notifier"
 	"github.com/wizelineacademy/GoWorkshop/shared"
 	"golang.org/x/net/context"
-	"log"
 )
 
 type Service struct{}
@@ -13,6 +14,8 @@ func (s *Service) NewUser(ctx context.Context, in *notifier.NewUserRequest) (*no
 	err := shared.SendEmail(in.Email, "Welcome to Wizeline Golang Workshop!")
 	if err != nil {
 		log.Println(err.Error())
+	} else {
+		log.Printf("NewUser email sent to %s\n", in.Email)
 	}
 
 	return &notifier.NewUserResponse{
